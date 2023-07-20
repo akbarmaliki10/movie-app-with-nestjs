d828b69e9075698be5057192d01f9ac6dca6300c
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-07-20 22:43:29 +0700

    add custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,12 @@ import { Controller, Post, UseGuards, Request, Get } from "@nestjs/common";
 import { LocalAuthGuard } from "./local.auth.guard";
 import { AuthService } from "./auth.service";
 import { JwtAuthGuard } from "./jwt-auth.guard";
+import { UserCustomDecorator } from "./user.decorator";
+import { User } from "src/users/users.service";
 
 @Controller()
 export class AuthController {
-    constructor(private readonly authService : AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
     @UseGuards(LocalAuthGuard)
     @Post('login')
@@ -15,7 +17,13 @@ export class AuthController {
 
     @UseGuards(JwtAuthGuard)
     @Get('protected')
-    getHello(@Request() req) : string {
+    getHello(@Request() req): string {
         return req.user;
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('getName')
+    async findOne(@UserCustomDecorator() user: User) {
+        return user;
     }
 }
