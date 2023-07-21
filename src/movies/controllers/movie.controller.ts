c9f343fe2272f0a8d2d89149dac68242e4318f49
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-07-21 21:38:55 +0700

    add file upload endpoint

--- src/movies/controllers/movie.controller.ts
+++ src/movies/controllers/movie.controller.ts
@@ -1,11 +1,12 @@
 import { ValidationPipe } from "@nestjs/common";
-import { Controller, Get, Post, Delete, Body, UsePipes } from "@nestjs/common/decorators";
+import { Controller, Get, Post, Delete, Body, UsePipes, UseInterceptors, UploadedFile } from "@nestjs/common/decorators";
+import { FileInterceptor } from "@nestjs/platform-express";
 import { CreateMovieDto } from "src/movies/dto/CreateMovie.dto";
 import { MovieService } from "src/movies/services/movie.service";
 
 @Controller("movies")
 export class MovieController {
-  constructor(private movieService : MovieService){};
+  constructor(private movieService: MovieService) { };
 
   @Get()
   getAllMovie() {
@@ -13,18 +14,24 @@ export class MovieController {
   }
 
   @Get(":id")
-  getMovieById(){
+  getMovieById() {
     return {}
   }
 
   @Post()
   @UsePipes(ValidationPipe) //for giving better explanation in response by validating request
-  createMovie(@Body() createMovieDto : CreateMovieDto){
+  createMovie(@Body() createMovieDto: CreateMovieDto) {
     return this.movieService.createMovieService(createMovieDto);
   }
 
   @Delete(":id")
-  deleteMovie(){
+  deleteMovie() {
     return []
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return file
   }
 }
